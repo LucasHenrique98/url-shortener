2871589da2294e95b3ba70f592b2a2b522dd7e23
LucasHenrique98 <henrique.lucas30@gmail.com>
2024-01-25 08:13:25 -0300

    feat: creates a handler file

---
+++ src/handler.js
@@ -0,0 +1,24 @@
+const html = `
+  <html>
+    <head>
+      <title>301 Moved Permanently</title>
+    </head>
+    <body>
+      <center>
+        <h1>301 Moved Permanently</h1>
+      </center>
+    </body>
+  </html>
+`;
+
+const redirect = (event) => {
+  const headers = {
+    Location: 'https://www.google.com',
+  };
+
+  return {
+    statusCode: 301,
+    headers,
+    body: html,
+  };
+}
